049490bcee975baf450490456e67c874008d1b66
MaxlaserTech <104206888+IlikeScript1234@users.noreply.github.com>
2023-07-31 20:56:22 +0700

    develop update

--- loadstring.lua
+++ loadstring.lua
@@ -1,5 +1,5 @@
 if shared.zylqdevelop or shared.NightbedPrivate then
-    warn("Now You Are In Zylq Beta Development
+    warn("Now You Are In Zylq Beta Development")
   else
     loadstring(game:HttpGet("https://raw.githubusercontent.com/ImNicknamez/zylq/main/loader.lua", true))()
 end
