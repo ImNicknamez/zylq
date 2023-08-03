6ca10a47a6702603caa9c96777d26e4484155040
MaxlaserTech <104206888+IlikeScript1234@users.noreply.github.com>
2023-08-03 15:53:19 +0700

    change

--- loadstring.lua
+++ loadstring.lua
@@ -1,5 +1,6 @@
 if shared.zylqdevelop or shared.NightbedPrivate then
     warn("Now You Are In Zylq Beta Development")
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/ImNicknamez/zylq/main/loader.lua", true))()
   else
     loadstring(game:HttpGet("https://raw.githubusercontent.com/ImNicknamez/zylq/main/loader.lua", true))()
 end
